fda1b0ca830c9a00405cda6104f5cd0bd9b96be7
SHOHAN <shohanurrahman2334@gmail.com>
2022-06-07 02:06:21 +0600

    index file add

---
+++ resources/views/index.blade.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+</head>
+
+<body>
+    <table class="table" style="width: 50%;margin:auto;">
+        <thead class="thead-dark">
+            <tr>
+                <th scope="col">Id</th>
+                <th scope="col">Date</th>
+                <th scope="col">List</th>
+                <th scope="col">Edit</th>
+                <th scope="col">Insert</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <th scope="row">1</th>
+                <td>3/5/2022</td>
+                <td>Potato,Meet</td>
+                <td>edit</td>
+                <td>insert</td>
+            </tr>
+            <tr>
+                <th scope="row">2</th>
+                <td>4/5/2022</td>
+                <td>Fish,vegetable</td>
+                <td>edit</td>
+                <td>insert</td>
+            </tr>
+            <tr>
+                <th scope="row">3</th>
+                <td>5/52022</td>
+                <td>chicken</td>
+                <td>edit</td>
+                <td>insert</td>
+            </tr>
+        </tbody>
+    </table>
+
+
+</body>
+
+</html>
