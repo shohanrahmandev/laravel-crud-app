df118d3a294eeeb3ff9c1518f82c092f0ec782de
SHOHAN <shohanurrahman2334@gmail.com>
2022-06-07 17:30:24 +0600

    edit  page some changr

--- resources/views/edit.blade.php
+++ resources/views/edit.blade.php
@@ -1,39 +1,56 @@
-<!DOCTYPE html>
+<!doctype html>
 <html lang="en">
 
 <head>
-    <title>Bootstrap Example</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+    <title>Crud App</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
 </head>
 
 <body>
 
-    <div class="container">
-        <form class="form-horizontal" action="index.php">
-            <div class="form-group" style="margin-top:10%;">
-                <label class="control-label col-sm-2">Date:</label>
-                <div class="col-sm-3">
-                    <input type="date" class="form-control" id="pwd" placeholder="Enter date" name="pwd">
+    <div class="d-flex justify-content-center mt-5 p-5">
+
+
+
+        <section style="width: 30%;">
+
+            <h1 class="d-flex justify-content-center mt-5 p-5"> Crud Insert App </h1>
+
+            <form class=" border shadow-lg p-3 mb-5 bg-body rounded">
+                <div class="mb-3 col-1">
+                    <label for="id" class="form-label"> ID : </label>
+                    <input type="number" class="form-control" id="exampleInputid">
                 </div>
-            </div>
-            <div class="form-group">
-                <label class="control-label col-sm-2" for="pwd">List :</label>
-                <div class=" col-sm-3">
-                    <input type="text" class="form-control" id="pwd" placeholder="Enter list" name="pwd">
+                <hr>
+                <div class="mb-3 col-3">
+                    <label for="date" class="form-label">Date :</label>
+                    <input type="date" class="form-control" id="date">
                 </div>
-            </div>
-            <div class="form-group">
-                <div class="col-sm-offset-2 col-sm-10">
-                    <button type="submit" class="btn btn-default">Submit</button>
+                <hr>
+                <div class="mb-3 col-5">
+                    <label for="text" class="form-label">List Food :</label>
+                    <input type="text" class="form-control" id="text">
                 </div>
-            </div>
-        </form>
+                <hr>
+
+                <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                </div>
+                <button type="submit" class="btn btn-success">Submit</button>
+            </form>
+
+        </section>
     </div>
 
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
