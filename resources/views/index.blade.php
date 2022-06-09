e347b20badb3ac836c1eb869bcb3ec807308f9eb
SHOHAN <shohanurrahman2334@gmail.com>
2022-06-09 17:12:34 +0600

    boostrap modal

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,15 +13,55 @@
 
     <div class="d-flex justify-content-center mt-5 p-5">
         <section style="width:50%;">
-            <div class="d-flex justify-content-center mt-5 p-5">
+            <div class="d-flex justify-content-center p-3">
                 <h1> Crud App</h1>
             </div>
-            <h4 class="text-center text-capitalize">
-                <a href="/homework" class="btn btn-success">Homework</a>
-            </h4>
-            <h4 class="text-center text-capitalize">
-                <a href="/create" class="btn btn-success">Create</a>
-            </h4>
+
+            <div class="text-center p-2">
+                <!-- Button trigger modal -->
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    Create
+                </button>
+
+                <!-- Modal -->
+                <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <div class="modal-body">
+                                <form action="/store">
+
+                                    <div class="mb-3">
+                                        <label for="date" class="form-label">Date :</label>
+                                        <input type="date" class="form-control" name="date" id="date">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="name" class="form-label"> Name : </label>
+                                        <input type="text" name="name" class="form-control" id="name">
+                                    </div>
+
+                                    <div class="mb-3">
+                                        <label for="list-food" class="form-label">List Food :</label>
+                                        <input type="text" name="food" class="form-control" id="list-food">
+                                    </div>
+
+                                    <div class="modal-footer">
+                                        <button type="submit" class="btn btn-primary">Save</button>
+                                    </div>
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--table-->
             <table class="table table-striped border shadow-lg p-5 mb-5 bg-body rounded ">
                 <thead class="table-success">
                     <tr>
